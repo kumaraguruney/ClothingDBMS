121f2c82621421a13ecfdf96f2ec500330cf9ce2
Kumaraguru <gurukarbonn@gmail.com>
2016-04-27 16:35:09 -0500

    inventory Design changes

--- ClothingDBMS/ClothingDBMS/InventoryManagement/Inventory.aspx.designer.cs
+++ ClothingDBMS/ClothingDBMS/InventoryManagement/Inventory.aspx.designer.cs
@@ -155,14 +155,5 @@ namespace ClothingDBMS.InventoryManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCancelInventory;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
