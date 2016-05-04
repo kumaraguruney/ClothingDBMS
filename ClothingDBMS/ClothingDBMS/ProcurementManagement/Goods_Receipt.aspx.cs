a38612d49053b9b561b645132fcf3261cab3a825
Kamesh <Kamesh.1319@gmail.com>
2016-05-03 21:59:02 -0500

    Goods_Receipt

--- ClothingDBMS/ClothingDBMS/ProcurementManagement/Goods_Receipt.aspx.cs
+++ ClothingDBMS/ClothingDBMS/ProcurementManagement/Goods_Receipt.aspx.cs
@@ -30,5 +30,33 @@ namespace ClothingDBMS.ProcurementManagement
             pnlAddGoodsReceipt.Visible = false;
             pnlGoodsReceipt.Visible = true;
         }
+        protected void btnaddGoods_Receipt_Click(object sender, EventArgs e)
+        {
+            PaneladdGoods_Receipt.Visible = true;
+            PanelgvGoods_Receipt.Visible = false;
+        }
+
+        protected void btnCancelGoods_Receipt_Click(object sender, EventArgs e)
+        {
+            PaneladdGoods_Receipt.Visible = false;
+            PanelgvGoods_Receipt.Visible = true;
+
+        }
+
+        protected void btnSaveGoods_Receipt_Click(object sender, EventArgs e)
+        {
+            SqlGoods_Receipt.InsertParameters["Name"].DefaultValue = txtGoods_ReceiptName.Text.ToUpper().Trim();
+            SqlGoods_Receipt.InsertParameters["Address"].DefaultValue = txtGoods_ReceiptAddress.Text.ToUpper().Trim();
+
+            SqlGoods_Receipt.Insert();
+            gvGoods_Receipt.DataBind();
+            PaneladdGoods_Receipt.Visible = false;
+            PanelgvGoods_Receipt.Visible = true;
+            txtGoods_ReceiptName.Text = string.Empty;
+            txtGoods_ReceiptAddress.Text = string.Empty;
+
+        }
     }
+
 }
+   
